5a90482799a15ec679ba78574e7249bc29ca6da5
Gaurav Sarage <gauravsarage.1902@gmail.com>
2023-11-04 22:52:39 +0530

    created a dialog to open new note, cancel or submit it and added a title and description to it

--- src/components/CreateNoteDialog.tsx
+++ src/components/CreateNoteDialog.tsx
@@ -1,13 +1,17 @@
 "use client";
-import React from 'react'
-import { Dialog, DialogTrigger } from './ui/dialog';
-import { Plus } from "lucide-react"
 
-interface Props {
-    
-}
+import React from 'react'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from './ui/dialog';
+import { Plus } from "lucide-react";
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+
+type Props = {};
 
 const CreateNoteDialog = (props: Props) => {
+    const [input, setInput] = React.useState("");
+
+
     return(
         <Dialog>
             <DialogTrigger>
@@ -16,6 +20,28 @@ const CreateNoteDialog = (props: Props) => {
                     <h2 className='font-semibold text-green-600 sm:mt-2'>New Note Book</h2>
                 </div>
             </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        New Note Book
+                    </DialogTitle>
+                    <DialogDescription>
+                        You can create a new note by clicking the button below
+                    </DialogDescription>
+                </DialogHeader>
+                <form>
+                    <Input value={input} onChange={(e) => setInput(e.target.value)} placeholder='Name..' />
+                    <div className="h-4"></div>
+                    <div className='flex items-center gap-2.5'>
+                        <Button type='reset' variant={'secondary'}>
+                            Cancel
+                        </Button>
+                        <Button className='bg-green-600' type='submit'>
+                            Create
+                        </Button>
+                    </div>
+                </form>
+            </DialogContent>
         </Dialog>
     )
 }
